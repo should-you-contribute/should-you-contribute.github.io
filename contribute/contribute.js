afe5eafe5e5b9fb7cd96708938958abf11705a96
Shauna <shaunagm@gmail.com>
2015-06-18 17:14:21 -0400

    Add Github API request onclick

--- contribute/contribute.js
+++ contribute/contribute.js
@@ -1,16 +1,53 @@
+$(document).ready(function() {
+
+    $("#repo_submit").click(function(e) {
+        e.preventDefault();
+        makeCorsRequest();
+    });
+
+});
 
 
-function get_github_info() {
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
 
-        $.ajax({
-            url: "https://api.github.com/repos/" + $("#repo_name").val(),
-            dataType: 'jsonp',
-            jsonp: 'callback',
-            jsonpCallback: 'jsonpCallback',
-            success: function(){
-                alert("success");
-            }
-        });
+function makeCorsRequest() {
 
+    var url = "https://api.github.com/repos/" + $("#repo_name").val();
 
-};
+    var xhr = createCORSRequest('GET', url);
+
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    // Waits for request response before executing.
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) { // If the HTTP request has completed
+            if (this.status == 200) { // If the HTTP response code is 200 (e.g. successful)
+                // Response handlers.
+                console.log(xhr.responseText);
+            };
+        };
+    };
+
+    xhr.onerror = function() {
+        alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
+}
