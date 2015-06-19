85577c844f1b53dc47e171a1e899143eaf4c7aba
Shauna <shaunagm@gmail.com>
2015-06-19 14:34:18 -0400

    Refactor contribute.js to abstract out request logic

--- contribute/contribute.js
+++ contribute/contribute.js
@@ -1,34 +1,62 @@
 $(document).ready(function() {
 
     $("#repo_submit").click(function(e) {
+
         e.preventDefault();
-        makeCorsRequest();
+        var xhr = makeCorsRequest("https://api.github.com/repos/"
+            + $("#repo_name").val(), parse_initial_request);
     });
 
 });
 
 
-function createCORSRequest(method, url) {
+
+function parse_initial_request(xhr) {
+
+    if (xhr.status == 404) {
+        $("#repo_error").text("That is not a valid, public Github repository.  Please try again.");
+    };
+
+    if (xhr.status == 403) {
+        $("#repo_error").text("Too many people are using this site.  Please try again later.");
+    };
+
+    if (xhr.status == 200) {
+        $("#repo_error").text("Success!");
+        parse_github_response(xhr);
+    };
+
+}
+
+
+function parse_github_response(xhr) {
+
+    console.log("Surely we amde it his far");
+
+    var data = jQuery.parseJSON(xhr.responseText);
+
+    // Add repo name to title
+    $('#checklist_title').prepend(data.full_name + "'s ");
+
+    console.log(data);
+
+}
+
+function makeCorsRequest(url, callback) {
+
     var xhr = new XMLHttpRequest();
+
     if ("withCredentials" in xhr) {
         // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
+        xhr.open('GET', url, true);
     } else if (typeof XDomainRequest != "undefined") {
         // XDomainRequest for IE.
         xhr = new XDomainRequest();
-        xhr.open(method, url);
+        xhr.open('GET', url);
     } else {
         // CORS not supported.
         xhr = null;
     }
-    return xhr;
-}
-
-function makeCorsRequest() {
-
-    var url = "https://api.github.com/repos/" + $("#repo_name").val();
-
-    var xhr = createCORSRequest('GET', url);
 
     if (!xhr) {
         alert('CORS not supported');
@@ -36,24 +64,9 @@ function makeCorsRequest() {
     }
 
     // Waits for request response before executing.
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function ( ) {
         if (this.readyState == 4) { // If the HTTP request has completed
-
-            // 404 -- try again
-            if (this.status == 404) {
-                $("#repo_error").text("That is not a valid, public Github repository.  Please try again.");
-            };
-
-            // 403 -- rate limit exceeded
-            if (this.status == 403) {
-                $("#repo_error").text("Too many people are using this site.  Please try again later.");
-            };
-
-            // 200 -- yay parsing time
-            if (this.status == 200) {
-                $("#repo_error").text("Success!");
-                parse_github_response(xhr);
-            };
+            callback(this);
         };
     };
 
@@ -62,15 +75,5 @@ function makeCorsRequest() {
     };
 
     xhr.send();
-}
-
-function parse_github_response(xhr) {
-
-    var data = jQuery.parseJSON(xhr.responseText);
-
-    // Add repo name to title
-    $('#checklist_title').prepend(data.full_name + "'s ");
-
-    console.log(data);
 
 }
